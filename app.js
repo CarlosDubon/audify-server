1bf91385121dd0d98206c9e672b03a6f81c0d15c
Douglas Hernández <00004516@uca.edu.sv>
2021-07-28 17:57:21 -0600

    Update: middleware of main routers

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(["/api", "/api/v1", "/v1"], apiRouter);
+app.use(["/api/v1", "/api", "/v1"], apiRouter);
 app.use(errorHandler);
 
 module.exports = app;
